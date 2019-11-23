13ce97911634a480b8405066ffa0f6f1c538257f
Titu <titu017@gmail.com>
2019-11-23 23:40:46 +0600

    product webview is done with data and action bar

--- app/src/main/java/com/dcccul/dharendacredit/product/ProductDetailsActivity.java
+++ app/src/main/java/com/dcccul/dharendacredit/product/ProductDetailsActivity.java
@@ -3,13 +3,19 @@ package com.dcccul.dharendacredit.product;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.dcccul.dharendacredit.R;
 
 public class ProductDetailsActivity extends AppCompatActivity {
+
+    android.support.v7.widget.Toolbar toolbar;
     TextView productName;
     Context context;
     Bundle extra;
@@ -22,6 +28,16 @@ public class ProductDetailsActivity extends AppCompatActivity {
 
         webView = findViewById(R.id.webViewId_inProductDetailsActivity);
         context = ProductDetailsActivity.this;
+
+        //   >>>>    toolbar section starts here
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+
+        toolbar.setPadding(0,15,0,0);
+        toolbar.setContentInsetsAbsolute(0,0);
+
+        //  <<<<    toolbar section ends here
 
         extra = getIntent().getExtras();
 
@@ -40,5 +56,26 @@ public class ProductDetailsActivity extends AppCompatActivity {
             webSettings.setJavaScriptEnabled(true);
 
         }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()== R.id.noticeItemId){
+
+            Toast.makeText(this, "Notice Clicked", Toast.LENGTH_SHORT).show();
+
+        } else if (item.getItemId()== R.id.noticeItemId) {
+
+            Toast.makeText(this, "Notice Clicked", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
