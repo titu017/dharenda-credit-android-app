7ebd74c876c0e500c9973bfab719ca21c04cf47c
Titu <titu017@gmail.com>
2019-11-25 02:47:53 +0600

    board of directors is done

---
+++ app/src/main/java/com/dcccul/dharendacredit/staff/StaffActivity.java
@@ -0,0 +1,15 @@
+package com.dcccul.dharendacredit.staff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dcccul.dharendacredit.R;
+
+public class StaffActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staff);
+    }
+}
